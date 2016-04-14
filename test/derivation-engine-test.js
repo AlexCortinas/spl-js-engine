e2d9044b27a992db309e5b0c09dadac2297fb75f
Álex Cortiñas <mail@alexcortinas.es>
2016-04-14 18:26:42 +0200

    Creating code folder form test SimpleSPL

--- test/derivation-engine-test.js
+++ test/derivation-engine-test.js
@@ -13,10 +13,10 @@ before(removeTmpFolder);
 test('Create a project without feature selection or any custom data', () => {
     const engine = new DerivationEngine();
 
-    engine.generateProject(p('simpleSPL'), p('tmp/simpleSPL'));
+    engine.generateProject(p('simpleSPL/code'), p('tmp/simpleProduct'));
 
-    assert.strictEqual(f('simpleSPL/index.html'), f('tmp/simpleSPL/index.html'));
-    assert.strictEqual(f('simpleSPL/main.js'), f('tmp/simpleSPL/main.js'));
+    assert.strictEqual(f('simpleSPL/code/index.html'), f('tmp/simpleProduct/index.html'));
+    assert.strictEqual(f('simpleSPL/code/main.js'), f('tmp/simpleProduct/main.js'));
 });
 
 test('Create a project', () => {
