281c37a7e0ba779f1329a0e467405254e72eb28d
Álex Cortiñas <mail@alexcortinas.es>
2016-07-08 14:58:44 +0200

    Added consistency report when using cli with errors and warnings

--- src/cli.js
+++ src/cli.js
@@ -39,5 +39,30 @@ export function cli() {
         productJson = readJsonFromFile(product);
     }
 
+    const report = engine.analyseAnnotations();
+    const featuresConsistency = report.checkAnnotatedFeaturesConsistency();
+    const dataConsistency = report.checkAnnotatedDataConsistency(productJson);
+
+    if (featuresConsistency.errors || dataConsistency.errors) {
+        console.log('Inconsistency errors!');
+        console.log('============================');
+        console.log('Features consistency:');
+        console.log(featuresConsistency);
+        console.log('============================');
+        console.log('Data parameters consistency:');
+        console.log(dataConsistency);
+        console.log('============================');
+        return;
+    } else if (featuresConsistency.warnings || dataConsistency.warnings) {
+        console.log('Inconsistency warnings!');
+        console.log('============================');
+        console.log('Features consistency:');
+        console.log(featuresConsistency);
+        console.log('============================');
+        console.log('Data parameters consistency:');
+        console.log(dataConsistency);
+        console.log('============================');
+    }
     engine.generateProduct(output, productJson);
+    console.log(`Product generated at ${output}`);
 }
