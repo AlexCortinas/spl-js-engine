423f8d37ef346b4bd10799dc0a57ee1be49e55f3
Álex Cortiñas <mail@alexcortinas.es>
2017-06-27 13:00:50 +0200

    Updating example MyCalculator
    Eliminamos los ficheros yaml, ya que estamos dejando de dar soporte
    paulatinamente.
    También adapto run.sh a la nueva forma de llamar al cliente del motor de
    derivación.

--- examples/MyCalculator/run.sh
+++ examples/MyCalculator/run.sh
@@ -1,3 +1,3 @@
 #! /bin/env bash
 
-spl-js-engine --featureModel model.yaml --product product.yaml --config config.yaml --code code --output output
+spl-js-engine --featureModel model.json --product product.json --config config.json --extra extra.js --code code --output output
