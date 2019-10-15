b6efb6e89d857bf049dbac94b124f8b5626e705d
Álex Cortiñas <mail@alexcortinas.es>
2019-10-15 13:21:23 +0200

    Fixing null checks on FeatureSelectionError

--- src/feature-model/feature-selection-error.js
+++ src/feature-model/feature-selection-error.js
@@ -1,10 +1,14 @@
 export default class FeatureSelectionError {
   constructor(message, featureSelection, previousException) {
     this.message = message;
-    this.featureSelection = featureSelection;
-    this.featureName = previousException.featureName;
-    this.featureType = previousException.featureType;
-    this.errorType = previousException.errorType;
+    if (featureSelection) {
+      this.featureSelection = featureSelection;
+    }
+    if (previousException) {
+      this.featureName = previousException.featureName;
+      this.featureType = previousException.featureType;
+      this.errorType = previousException.errorType;
+    }
   }
 
   toString() {
