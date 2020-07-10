8bfb1e6a659cf02f806a08e0d541f0696cd4f554
Álex Cortiñas <mail@alexcortinas.es>
2020-07-10 13:08:40 +0200

    Adding tests for local and zip combinations

--- test/file-generation-test.js
+++ test/file-generation-test.js
@@ -1,6 +1,8 @@
 import {DerivationEngine, readJsonFromFile, readFile} from '../src/index';
 import {getTestPath as p, removeTmpFolder, assertEqualFilesInFolders} from './test-utils';
 import JSZip from 'jszip';
+import fs from 'fs';
+import path from 'path';
 
 suite('#FileGeneration - checks that using zip or local files is the sames');
 
@@ -38,3 +40,71 @@ test('Generate local from zip', (done) => {
     });
   }).catch(e => done(e));
 });
+
+test('Generate local from zip with a folder inside', (done) => {
+  JSZip.loadAsync(readFile(p('simpleSPLwithFolder.zip'), true)).then((zip) => {
+    return new DerivationEngine({
+      zip,
+      featureModel: 'simpleSPL/model.json',
+      codePath: 'simpleSPL/code',
+      config: 'simpleSPL/config.json'
+    }).then((engine) => {
+      engine.generateProduct(
+        p('tmp/simpleProduct'),
+        readJsonFromFile(p('simpleSPL/product.json'))
+      ).then(() => {
+        assertEqualFilesInFolders(p('simpleSPL/expected'), p('tmp/simpleProduct'));
+        done();
+      });
+    });
+  }).catch(e => done(e));
+});
+
+test('Generate zip from zip', (done) => {
+  JSZip.loadAsync(readFile(p('simpleSPL.zip'), true)).then((zip) => {
+    new DerivationEngine({ zip, featureModel: 'model.json' }).then(engine => {
+      engine.generateZip(readJsonFromFile(p('simpleSPL/product.json'))).then((outputZipFile) => {
+        fs.mkdirSync(p('tmp/simpleProduct'), { recursive: true });
+        const promises = [];
+        Object.keys(outputZipFile.files)
+          .filter(fPath => !outputZipFile.files[fPath].dir)
+          .forEach((fPath) => {
+            fs.mkdirSync(p('tmp/simpleProduct/' + path.dirname(fPath)), { recursive: true });
+            promises.push(outputZipFile.files[fPath].async('nodebuffer').then((fContent) => {
+              fs.writeFileSync(p('tmp/simpleProduct/') + fPath, fContent);
+            }));
+          });
+        Promise.all(promises).then(() => {
+          assertEqualFilesInFolders(p('simpleSPL/expected'), p('tmp/simpleProduct'));
+          done();
+        });
+      });
+    });
+  }).catch(e => done(e));
+});
+
+test('Generate zip from local', (done) => {
+  new DerivationEngine({
+    codePath: p('simpleSPL/code'),
+    featureModel: readJsonFromFile(p('simpleSPL/model.json')),
+    config: readJsonFromFile(p('simpleSPL/config.json'))
+  }).then(engine => {
+    engine.generateZip(readJsonFromFile(p('simpleSPL/product.json'))).then((outputZipFile) => {
+      fs.mkdirSync(p('tmp/simpleProduct'), { recursive: true });
+      const promises = [];
+      Object.keys(outputZipFile.files)
+        .filter(fPath => !outputZipFile.files[fPath].dir)
+        .forEach((fPath) => {
+          fs.mkdirSync(p('tmp/simpleProduct/' + path.dirname(fPath)), { recursive: true });
+          promises.push(outputZipFile.files[fPath].async('nodebuffer').then((fContent) => {
+            fs.writeFileSync(p('tmp/simpleProduct/') + fPath, fContent);
+          }));
+        });
+      Promise.all(promises).then(() => {
+        assertEqualFilesInFolders(p('simpleSPL/expected'), p('tmp/simpleProduct'));
+        done();
+      });
+    });
+  }).catch(e => done(e));
+});
+
