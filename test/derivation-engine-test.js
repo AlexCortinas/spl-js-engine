1033088364c82597d44f306e4d6d9e236f0ae00b
Álex Cortiñas <mail@alexcortinas.es>
2022-06-23 11:54:31 +0200

    Splitting derivation engine test into two files

--- test/derivation-engine-test.js
+++ test/derivation-engine-test.js
@@ -1,4 +1,3 @@
-import assert from 'assert';
 import {DerivationEngine, readJsonFromFile, readFile} from '../src/index';
 import {getTestPath as p, removeTmpFolder, assertEqualFilesInFolders} from './test-utils';
 
@@ -62,122 +61,6 @@ test('Create a product and use some extra js function', async () => {
   assertEqualFilesInFolders(p('simpleSPLwithExtraJS/expected'), p('tmp/simpleProduct'));
 });
 
-test('Get features and parameters of an annotated project', async () => {
-  const engine = await new DerivationEngine(p('simpleSPLwithData/code'));
-
-  engine.setConfig(readJsonFromFile(p('simpleSPLwithData/config.json')));
-
-  const report = await engine.analyseAnnotations();
-
-  assert.deepEqual(
-    report.short(),
-    {
-      feature: {featureA: 2, featureB: 1, featureC: 1},
-      data: {aValue: 1, bValue: 1}
-    }
-  );
-
-  assert.deepEqual(
-    report.long(),
-    {
-      'index.html': {
-        feature: {featureA: 1, featureB: 1, featureC: 1},
-        data: {}
-      },
-      'main.js': {
-        feature: {featureA: 1},
-        data: {aValue: 1, bValue: 1}
-      }
-    }
-  );
-
-  assert.deepEqual(
-    report.filesByFeature('featureA'),
-    ['index.html', 'main.js']
-  );
-
-  assert.deepEqual(
-    report.filesByFeature('featureB'),
-    ['index.html']
-  );
-
-  assert.deepEqual(
-    report.filesByData('aValue'),
-    ['main.js']
-  );
-});
-
-test('Compare feature model vs analysed code results', async () => {
-  const engine = await new DerivationEngine(
-    p('simpleSPLwithData/code'),
-    readJsonFromFile(p('simpleSPLwithData/model.json')),
-    readJsonFromFile(p('simpleSPLwithData/config.json'))
-  );
-
-  const report = await engine.analyseAnnotations();
-
-  assert.deepEqual(
-    report.checkAnnotatedFeaturesConsistency(),
-    {
-      surplus: {
-        desc: 'features found in code but not in feature model',
-        values: [],
-        count: 0
-      },
-      missing: {
-        desc: 'child features found in feature model but not in code',
-        values: [],
-        count: 0
-      }
-    }
-  );
-
-  assert.deepEqual(
-    report.checkAnnotatedDataConsistency(
-      readJsonFromFile(p('simpleSPLwithData/product.json'))
-    ),
-    {errors: 0, warnings: 0, surplus: [], missing: []}
-  );
-});
-
-
-test('Compare fanalysis results in a not consistent project', async () => {
-  const engine = await new DerivationEngine(
-    p('simpleSPLwrong/code'),
-    readJsonFromFile(p('simpleSPLwrong/model.json')),
-    readJsonFromFile(p('simpleSPLwrong/config.json'))
-  );
-
-  const report = await engine.analyseAnnotations();
-
-  assert.deepEqual(
-    report.checkAnnotatedFeaturesConsistency(),
-    {
-      surplus: {
-        desc: 'features found in code but not in feature model',
-        values: ['featureWrong', 'anotherFeatureWrong'],
-        count: 2
-      },
-      missing: {
-        desc: 'child features found in feature model but not in code',
-        values: ['featureA'],
-        count: 1
-      }
-    }
-  );
-
-  assert.deepEqual(
-    report.checkAnnotatedDataConsistency(
-      readJsonFromFile(p('simpleSPLwrong/product.json'))
-    ),
-    {
-      errors: 2, warnings: 1,
-      surplus: ['wrongValue', 'anotherWrongValue'],
-      missing: ['aValue']
-    }
-  );
-});
-
 test('Create a product with data parameters at several levels', async () => {
   const engine = await new DerivationEngine(
     p('simpleSPLwithDataComplex/code'),
@@ -194,38 +77,28 @@ test('Create a product with data parameters at several levels', async () => {
     p('simpleSPLwithDataComplex/expected'), p('tmp/simpleProduct'));
 });
 
-test('Consistency results for complex data', async () => {
-  const engine = await new DerivationEngine(
-    p('simpleSPLwithDataComplex/code'),
-    readJsonFromFile(p('simpleSPLwithDataComplex/model.json')),
-    readJsonFromFile(p('simpleSPLwithDataComplex/config.json'))
-  );
+test('Model transformation', (done) => {
+  new DerivationEngine(
+    p('simpleSPLwithTransformation/code'),
+    readJsonFromFile(p('simpleSPLwithTransformation/model.json')),
+    readJsonFromFile(p('simpleSPLwithTransformation/config.json')),
+    '',
+    readFile(p('simpleSPLwithTransformation/transformation.js'))
+  ).then(engine => {
+    engine.generateProduct(
+      p('tmp/simpleProduct'),
+      readJsonFromFile(p('simpleSPLwithTransformation/product.json'))
+    );
 
-  const report = await engine.analyseAnnotations();
-
-  assert.deepEqual(
-    report.checkAnnotatedFeaturesConsistency(),
-    {
-      surplus: {
-        desc: 'features found in code but not in feature model',
-        values: [],
-        count: 0
-      },
-      missing: {
-        desc: 'child features found in feature model but not in code',
-        values: [],
-        count: 0
-      }
-    }
-  );
-
-  assert.deepEqual(
-    report.checkAnnotatedDataConsistency(
-      readJsonFromFile(p('simpleSPLwithDataComplex/product.json'))
-    ),
-    {errors: 0, warnings: 0, surplus: [], missing: []}
-  );
+    assertEqualFilesInFolders(p('simpleSPLwithTransformation/expected'), p('tmp/simpleProduct'));
+    done();
+  });
 });
+
+suite('#DerivationEngine: Binay files and no extension files');
+
+beforeEach(removeTmpFolder);
+afterEach(removeTmpFolder);
 
 test('Checking "binarity" of unknown extensions not in config', async () => {
   const engine = await new DerivationEngine(
@@ -274,32 +147,6 @@ test('Create a product with a file without extension', async () => {
   assertEqualFilesInFolders(p('simpleSPLwithDockerfile/expected'), p('tmp/simpleProduct'));
 });
 
-suite('#DerivationEngine: Analysis tools');
-
-beforeEach(removeTmpFolder);
-afterEach(removeTmpFolder);
-
-test('Get files with more features involved', async () => {
-  const engine = await new DerivationEngine(
-    'examples/GPL/GraphProductLine',
-    readJsonFromFile('examples/GPL/model.json'),
-    readJsonFromFile('examples/GPL/config.json')
-  );
-
-  const report = await engine.analyseAnnotations();
-
-  // TODO make a properly test
-  report.short();
-  report.long();
-  report.filesByFeature('weighted');
-  report.filesByFeatureLong('weighted', true);
-  // fails on windows due to the separation /
-  //report.featuresByFile('src/gpl/graph/Graph.java', true);
-  report.filesByData('asdf');
-  report.listFeatures(true);
-  report.listFiles(true);
-});
-
 suite('#DerivationEngine: Generation of files');
 
 beforeEach(removeTmpFolder);
@@ -335,7 +182,7 @@ test('Create folder', async () => {
   assertEqualFilesInFolders(p('spl-generate-folder/expected'), p('tmp/simpleProduct'));
 });
 
-test('Checking escape of first character of end delimiter', async () => {
+test('Using first character of end delimiter in the filename of generated files', async () => {
   const engine = await new DerivationEngine(
     p('spl-generate-escape/code'),
     readJsonFromFile(p('spl-generate-escape/model.json')),
@@ -378,22 +225,4 @@ test('Omitting empty files with double annotation', async () => {
   );
 
   assertEqualFilesInFolders(p('simpleSPLwithEmptyFiles2/expected'), p('tmp/simpleProduct'));
-});
-
-test('Model transformation', (done) => {
-  new DerivationEngine(
-    p('simpleSPLwithTransformation/code'),
-    readJsonFromFile(p('simpleSPLwithTransformation/model.json')),
-    readJsonFromFile(p('simpleSPLwithTransformation/config.json')),
-    '',
-    readFile(p('simpleSPLwithTransformation/transformation.js'))
-  ).then(engine => {
-    engine.generateProduct(
-      p('tmp/simpleProduct'),
-      readJsonFromFile(p('simpleSPLwithTransformation/product.json'))
-    );
-
-    assertEqualFilesInFolders(p('simpleSPLwithTransformation/expected'), p('tmp/simpleProduct'));
-    done();
-  });
 });
